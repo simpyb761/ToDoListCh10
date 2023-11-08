2f3f084e3a05dc2954bd4ce1e841e10284e2a674
simpyb761 <simpyb761@gmail.com>
2023-11-08 16:33:05 -0500

    Completed

---
+++ Ch10Ex1ToDoList-master/Ch10Ex1ToDoList-master/ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+        [ValidateNever]
+        public Filters Filters { get; set; } = null!;
+        [ValidateNever]
+        public List<Status> Statuses { get; set; } = null!;
+        [ValidateNever]
+        public List<Category> Categories { get; set; } = null!;
+        [ValidateNever]
+        public Dictionary<string, string> DueFilters { get; set; } = null!;
+        [ValidateNever]
+        public List<ToDo> Tasks { get; set; } = null!;
+        public ToDo CurrentTask { get; set; } = null!;
+    }
+}
